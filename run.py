8451606b1a4e11e644d2ef481ab19572d1e25318
Niall <niallos11@gmail.com>
2022-06-05 13:53:49 +0000

    commit -m create get sales data functions to get data string from user

--- run.py
+++ run.py
@@ -24,4 +24,16 @@ def get_sales_data():
     print(f"The data provided is {data_str}")
 
 
+def get_sales_data():
+    """
+    Get sales figures input from the user.
+    """
+    print("Please enter sales data from the last market.")
+    print("Data should be six numbers, separated by commas.")
+    print("Example: 10,20,30,40,50,60\n")
+
+    data_str = input("Enter your data here: ")
+    print(f"The data provided is {data_str}")
+
+
 get_sales_data()
